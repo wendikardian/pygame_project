be50e9ac71ed1aac48b40129622c3e52a9af5bd2
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-25 16:19:09 +0700

    ;:

--- Flappy Bird _Try/mod.py
+++ Flappy Bird _Try/mod.py
@@ -2,7 +2,7 @@ import random # For generating random numbers
 import sys # We will use sys.exit to exit the program
 import pygame
 from pygame.locals import * # Basic pygame imports
-
+#
 # Global Variables for the game
 FPS = 32
 frame_size_x = 289
